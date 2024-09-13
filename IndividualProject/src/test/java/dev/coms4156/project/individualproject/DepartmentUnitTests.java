ed30b97f90ea737fca7806130c7f2cc79c0c32d9
Yanxi Chen <103548668+YC0101@users.noreply.github.com>
2024-09-13 15:53:43 -0400

    Feat/depart test (#3)
    * chore: Creating department class test and fix bugs. Coverage 100%
    
    * chore: Remove extra code.
    
    * chore: Fix check style prblem for depart/class along with their test code.

---
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/DepartmentUnitTests.java
@@ -0,0 +1,113 @@
+package dev.coms4156.project.individualproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * This class contains tests for the Department class.
+ */
+@SpringBootTest
+@ContextConfiguration
+public class DepartmentUnitTests {
+
+  /**
+   * Method that setup all the test variable we need.
+   */
+  @BeforeAll
+  public static void setupDepartmentForTesting() {
+    testCourse = new Course("Kenneth Shepard", "1205 MUDD", "4:10-6:40", 45);
+    testCourse2 = new Course("David G Vallancourt", "301 PUP", "4:10-5:25", 33);
+    testCourse3 = new Course("Keren Bergman", "829 MUDD", "2:40-3:55", 45);
+
+    testCourseMap = new HashMap<>();
+    testCourseMaptoString = new HashMap<>();
+
+    testCourseMaptoString.put("1001", testCourse);
+    testCourseMap.put("1001", testCourse);
+    testCourseMap.put("1002", testCourse2);
+    testDept = new Department("3082", testCourseMap, "Luca Carloni", 4);
+    testDept2 = new Department("3082", testCourseMap, "Luca Carloni", 4);
+    testDept3 = new Department("3082", testCourseMap, "Luca Carloni", 4);
+    testDept4 = new Department("3082", testCourseMap, "Luca Carloni", 4);
+    testDeptToString = new Department("3082", testCourseMaptoString, "Luca Carloni", 4);
+  }
+
+  @Test
+  public void getNumberOfMajorsTest() {
+    assertEquals(4, testDept.getNumberOfMajors());
+  }
+
+  @Test
+  public void getDepartmentChairTest() {
+    assertEquals("Luca Carloni", testDept.getDepartmentChair());
+  }
+
+  @Test
+  public void getCourseSelectionTest() {
+    assertEquals(testCourseMap, testDept.getCourseSelection());
+  }
+
+  @Test
+  public void addPersonToMajorTest() {
+    assertEquals(4, testDept2.getNumberOfMajors());
+    testDept2.addPersonToMajor();
+    assertEquals(5, testDept2.getNumberOfMajors());
+  }
+
+  @Test
+  public void dropPersonFromMajorTest() {
+    testDept3.dropPersonFromMajor();
+    assertEquals(3, testDept3.getNumberOfMajors());
+    testDept3.dropPersonFromMajor();
+    testDept3.dropPersonFromMajor();
+    testDept3.dropPersonFromMajor();
+    assertEquals(0, testDept3.getNumberOfMajors());
+    testDept3.dropPersonFromMajor();
+    assertEquals(0, testDept3.getNumberOfMajors());
+  }
+
+  @Test
+  public void addCourseTest() {
+    testDept3.addCourse("1003", testCourse3);
+    HashMap<String, Course> expectedResult = new HashMap<>();
+    expectedResult.put("1001", testCourse);
+    expectedResult.put("1002", testCourse2);
+    expectedResult.put("1003", testCourse3);
+    assertEquals(expectedResult, testDept3.getCourseSelection());
+  }
+
+  @Test
+  public void createCourseTest() {
+    testDept4.createCourse("1003", "Keren Bergman", "829 MUDD", "2:40-3:55", 45);
+    HashMap<String, Course> expectedResult = new HashMap<>();
+    expectedResult.put("1001", testCourse);
+    expectedResult.put("1002", testCourse2);
+    expectedResult.put("1003", testCourse3);
+    assertEquals(expectedResult.toString(), testDept2.getCourseSelection().toString());
+  }
+
+  @Test
+  public void toStringTest() {
+    String expectedResult = "3082 1001: \n"
+            + "Instructor: Kenneth Shepard; Location: 1205 MUDD; Time: 4:10-6:40\n";
+    assertEquals(expectedResult, testDeptToString.toString());
+  }
+
+  /** The test department instance and course hashmap used for testing. */
+  public static Department testDept;
+  public static Department testDept2;
+  public static Department testDept3;
+  public static Department testDept4;
+  public static Department testDeptToString;
+  /** The test course instance used for testing. */
+  public static Course testCourse;
+  public static Course testCourse2;
+  public static Course testCourse3;
+  public static HashMap<String, Course> testCourseMap;
+  public static HashMap<String, Course> testCourseMaptoString;
+}
