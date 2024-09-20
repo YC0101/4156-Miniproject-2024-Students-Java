9b02d50316e6e238eef47672685426acda3821a4
ireneisabug <yc3841@columbia.edu>
2024-09-20 00:47:13 -0400

    chore: Add the method enrollStudentInCourse

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/RouteController.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/RouteController.java
@@ -435,6 +435,48 @@ public class RouteController {
   }
 
   /**
+   * Attempts to enroll a student from the specified course. If the course is full,
+   * the student won't get enrolled (BAD_REQUEST).
+   *
+   * @param deptCode       A {@code String} representing the department.
+   *
+   * @param courseCode     A {@code int} representing the course within the department.
+   *
+   * @return               A {@code ResponseEntity} object containing an HTTP 200
+   *                       response with an appropriate message or the proper status
+   *                       code in tune with what has happened.
+   */
+  @PatchMapping(value = "/enrollStudentInCourse", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> enrollStudentInCourse(
+          @RequestParam(value = "deptCode") String deptCode,
+          @RequestParam(value = "courseCode") int courseCode) {
+    try {
+      boolean doesCourseExists;
+      doesCourseExists = retrieveCourse(deptCode, courseCode).getStatusCode() == HttpStatus.OK;
+
+      if (doesCourseExists) {
+        HashMap<String, Department> departmentMapping;
+        departmentMapping = IndividualProjectApplication.myFileDatabase.getDepartmentMapping();
+        HashMap<String, Course> coursesMapping;
+        coursesMapping = departmentMapping.get(deptCode).getCourseSelection();
+
+        Course requestedCourse = coursesMapping.get(Integer.toString(courseCode));
+        boolean isStudentEnrolled = requestedCourse.enrollStudent();
+
+        if (isStudentEnrolled) {
+          return new ResponseEntity<>("Student has been enrolled.", HttpStatus.OK);
+        } else {
+          return new ResponseEntity<>("Student has not been enrolled.", HttpStatus.BAD_REQUEST);
+        }
+      } else {
+        return new ResponseEntity<>("Course Not Found", HttpStatus.NOT_FOUND);
+      }
+    } catch (Exception e) {
+      return handleException(e);
+    }
+  }
+
+  /**
    * Endpoint for changing the enrolled student count for the course.
    * This method handles PATCH requests to change the time of a course identified by
    * department code and course code.If the course exists, its time is updated to the 
