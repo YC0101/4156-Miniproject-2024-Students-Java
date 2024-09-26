fce530e2c918cad0859016c942f8faa9cfe019f5
Yanxi Chen <103548668+YC0101@users.noreply.github.com>
2024-09-26 18:35:16 -0400

    Feat/i3 tests (#13)
    * chore: Add tests
    
    * chore: Add README
    
    * chore: Remove extra import

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
@@ -1,16 +1,8 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.HashMap;
-import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
